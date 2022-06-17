0b315cc55e0949b4c46408003242923e67439b14
Arcyvilk <blazemakerrr@gmail.com>
2022-06-17 20:06:55 +0200

    feat: 🎸 add more OUCH

--- src/assets/sounds/ouch/index.ts
+++ src/assets/sounds/ouch/index.ts
@@ -10,6 +10,21 @@ import sound_9 from './09.mp3';
 import sound_10 from './10.mp3';
 import sound_11 from './11.mp3';
 import sound_12 from './12.mp3';
+import sound_13 from './13.mp3';
+import sound_14 from './14.mp3';
+import sound_15 from './15.mp3';
+import sound_16 from './16.mp3';
+import sound_17 from './17.mp3';
+import sound_18 from './18.mp3';
+import sound_19 from './19.mp3';
+import sound_20 from './20.mp3';
+
+export const getOuch = (): string => {
+  const min = Math.ceil(1);
+  const max = Math.floor(sounds.length);
+  const soundIndex = Math.floor(Math.random() * max) + min;
+  return sounds[soundIndex];
+};
 
 export const sounds: string[] = [
   sound_1,
@@ -24,11 +39,12 @@ export const sounds: string[] = [
   sound_10,
   sound_11,
   sound_12,
+  sound_13,
+  sound_14,
+  sound_15,
+  sound_16,
+  sound_17,
+  sound_18,
+  sound_19,
+  sound_20,
 ];
-
-export const getOuch = (): string => {
-  const min = Math.ceil(1);
-  const max = Math.floor(sounds.length);
-  const soundIndex = Math.floor(Math.random() * max) + min;
-  return sounds[soundIndex];
-};
