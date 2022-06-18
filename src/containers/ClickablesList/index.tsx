c837c7b5506fd71e37252783088b92a49f8187a0
Arcyvilk <blazemakerrr@gmail.com>
2022-06-18 23:40:07 +0200

    refactor: 💡 populate containers list

--- src/containers/ClickablesList/index.tsx
+++ src/containers/ClickablesList/index.tsx
@@ -1,16 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFingerprint } from '@fortawesome/free-solid-svg-icons';
 
+import { Flex, Modal, Title } from 'components';
+import { TrophyCabinet } from 'containers';
 import { useStoryModeContext } from 'shared/context';
-import { Modal } from 'components';
+import { useClickables } from 'data';
 
 export const ClickablesList = (): JSX.Element => {
   const { showClickablesList, setShowClickablesList } = useStoryModeContext();
+  const { unlockedClickables } = useClickables();
+
   return (
     <Modal visible={showClickablesList} setVisible={setShowClickablesList}>
-      <Wrapper>hehe</Wrapper>
+      <Wrapper>
+        <Title level={2}>
+          <FontAwesomeIcon
+            icon={faFingerprint}
+            style={{ marginRight: '8px' }}
+          />
+          Touchables
+        </Title>
+      </Wrapper>
+      <Trophies>
+        {unlockedClickables.map(clickable => (
+          <TrophyCabinet
+            key={`clickable-${clickable.id}`}
+            title={clickable.name}
+            description={clickable.description}
+            img={clickable.src}
+          />
+        ))}
+      </Trophies>
     </Modal>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled(Flex).attrs({ direction: 'column' })`
+  padding-bottom: 4px;
+  border-bottom: 1px solid;
+`;
+
+const Trophies = styled(Flex).attrs({ direction: 'column' })`
+  width: 100%;
+  & > * {
+    margin-top: 8px;
+  }
+`;
