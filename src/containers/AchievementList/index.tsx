bd2da7fb6ac2cb51cc18e6d2a5100212da726d1a
Arcyvilk <blazemakerrr@gmail.com>
2022-06-18 23:39:55 +0200

    refactor: 💡 populate achievement list

--- src/containers/AchievementList/index.tsx
+++ src/containers/AchievementList/index.tsx
@@ -1,18 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 import { useStoryModeContext } from 'shared/context';
-import { Modal } from 'components';
+import { Flex, Modal, Title } from 'components';
+import { TrophyCabinet } from 'containers';
+import { useAchievements } from 'data';
 
 export const AchievementList = (): JSX.Element => {
   const { showAchievementsList, setShowAchievementsList } =
     useStoryModeContext();
+  const { unlockedAchievements } = useAchievements();
 
   return (
     <Modal visible={showAchievementsList} setVisible={setShowAchievementsList}>
-      <Wrapper>hehe</Wrapper>
+      <Wrapper>
+        <Title level={2}>
+          <FontAwesomeIcon icon={faTrophy} style={{ marginRight: '8px' }} />
+          Achievements
+        </Title>
+      </Wrapper>
+      <Trophies>
+        {unlockedAchievements.map(achievement => (
+          <TrophyCabinet
+            key={`achievement-${achievement.id}`}
+            title={achievement.name}
+            description={achievement.description}
+            icon={faTrophy}
+          />
+        ))}
+      </Trophies>
     </Modal>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled(Flex).attrs({ direction: 'column' })`
+  padding-bottom: 4px;
+  border-bottom: 1px solid;
+`;
+
+const Trophies = styled(Flex).attrs({ direction: 'column' })`
+  width: 100%;
+  & > * {
+    margin-top: 8px;
+  }
+`;
