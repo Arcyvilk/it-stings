b3ddb006626c3a8a301b7c49e140dcfff5175927
Arcyvilk <blazemakerrr@gmail.com>
2022-06-18 20:18:42 +0200

    feat: 🎸 add new buttons to status bar

--- src/containers/StatusBar/index.tsx
+++ src/containers/StatusBar/index.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Flex } from 'components';
+import { Flex, IconButton } from 'components';
 import { useStoryModeContext } from 'shared/context';
+import {
+  faBookOpen,
+  faPowerOff,
+  faTrophy,
+} from '@fortawesome/free-solid-svg-icons';
 
 export const StatusBar = (): JSX.Element => {
   const { clicks, setClicks } = useStoryModeContext();
 
+  const onShowAchievements = () => {
+    alert('Achievements page is not implemented yet');
+  };
+  const onShowClickables = () => {
+    alert('Clickables page is not implemented yet');
+  };
   const onRestart = () => {
     setClicks(1);
   };
 
   return (
     <Bar align>
+      <Buttons>
+        <IconButton icon={faTrophy} onClick={onShowAchievements} />
+        <IconButton icon={faBookOpen} onClick={onShowClickables} />
+      </Buttons>
       <span>{clicks}</span>
-      <button onClick={onRestart}>Restart?</button>
+      <IconButton icon={faPowerOff} danger onClick={onRestart} />
     </Bar>
   );
 };
@@ -24,9 +39,15 @@ const Bar = styled(Flex)`
   justify-content: space-between;
   width: 100%;
   top: 0;
-  padding: 20px 32px;
+  padding: 8px;
   box-sizing: border-box;
   background-color: black;
   font-size: 1.5em;
   text-align: center;
+`;
+
+const Buttons = styled(Flex).attrs({ row: true })`
+  & > *:not(:last-child) {
+    margin-right: 8px;
+  }
 `;
