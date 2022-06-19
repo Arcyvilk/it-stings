c97e2e6e8ddd56551c526326db51851a1703e737
Arcyvilk <blazemakerrr@gmail.com>
2022-06-19 20:57:16 +0200

    style: 💄 better theming of story bar

--- src/containers/CactusStory/index.tsx
+++ src/containers/CactusStory/index.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useMemo } from 'react';
 import styled from 'styled-components';
 
-import { useStoryModeContext } from 'shared/context';
+import { useAppContext, useStoryModeContext } from 'shared/context';
 import { useSounds, useStory } from 'data';
+import { Theme } from 'shared/theme';
 
 export const CactusStory = (): JSX.Element => {
+  const { theme } = useAppContext();
   const { clicks } = useStoryModeContext();
   const { getStory } = useStory();
   const { playStorySound } = useSounds();
@@ -20,18 +22,18 @@ export const CactusStory = (): JSX.Element => {
   }, [clicks]);
 
   if (currentStory) {
-    return <Story>{currentStory.text}</Story>;
+    return <Story theme={theme}>{currentStory.text}</Story>;
   }
   return <></>;
 };
 
-const Story = styled.div`
+const Story = styled.div<{ theme: Theme }>`
   position: absolute;
   width: 100%;
   bottom: 0;
   padding: 12px 20px;
   box-sizing: border-box;
-  background-color: #000000bb;
+  background-color: ${({ theme }) => `${theme.tertiaryBg}dd`};
   font-size: 1.4em;
   text-align: center;
   line-height: 1.4em;
