fcbecfcb166423e870d8e6588a79e27606437c3f
Arcyvilk <blazemakerrr@gmail.com>
2022-06-19 20:56:32 +0200

    refactor: 💡 show settings button on status bar

--- src/containers/StatusBar/index.tsx
+++ src/containers/StatusBar/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Flex, IconButton } from 'components';
-import { useStoryModeContext } from 'shared/context';
-import {
-  faBookOpen,
-  faPowerOff,
-  faTrophy,
-} from '@fortawesome/free-solid-svg-icons';
+import { Theme } from 'shared/theme';
+import { Flex, IconButton, Title } from 'components';
+import { useAppContext, useStoryModeContext } from 'shared/context';
+import { faBookOpen, faCog, faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 export const StatusBar = (): JSX.Element => {
-  const { clicks, setClicks, setShowAchievementsList, setShowClickablesList } =
+  const { theme, setShowSettingsModal } = useAppContext();
+  const { clicks, setShowAchievementsList, setShowClickablesList } =
     useStoryModeContext();
 
   const onShowAchievements = () => {
@@ -19,35 +17,37 @@ export const StatusBar = (): JSX.Element => {
   const onShowClickables = () => {
     setShowClickablesList(true);
   };
-  const onRestart = () => {
-    setClicks(0);
+  const onShowSettings = () => {
+    setShowSettingsModal(true);
   };
 
   return (
-    <Bar align>
+    <Bar align theme={theme}>
       <Buttons>
         <IconButton icon={faTrophy} onClick={onShowAchievements} />
         <IconButton icon={faBookOpen} onClick={onShowClickables} />
       </Buttons>
-      <span>{clicks}</span>
-      <IconButton icon={faPowerOff} danger onClick={onRestart} />
+      <Title level={2}>{clicks}</Title>
+      <Buttons>
+        <IconButton icon={faCog} onClick={onShowSettings} />
+      </Buttons>
     </Bar>
   );
 };
 
-const Bar = styled(Flex)`
+const Bar = styled(Flex)<{ theme: Theme }>`
   position: absolute;
   justify-content: space-between;
   width: 100%;
   top: 0;
   padding: 8px;
   box-sizing: border-box;
-  background-color: black;
-  font-size: 1.5em;
+  background-color: ${({ theme }) => theme.tertiaryBg};
   text-align: center;
 `;
 
 const Buttons = styled(Flex).attrs({ row: true })`
+  flex: 0 0 auto;
   & > *:not(:last-child) {
     margin-right: 8px;
   }
