b8db9c9fe30b37adc005c239b3db7957da1477a1
Arcyvilk <blazemakerrr@gmail.com>
2022-06-19 18:57:16 +0200

    fix: 🐛 nerf clicks

--- src/data/story/story.ts
+++ src/data/story/story.ts
@@ -16,457 +16,457 @@ export const stories: Story[] = [
     id: '4393d0fd-e4fb-4e06-b733-d20e12739212',
   },
   {
-    clicks: 30,
+    clicks: 50,
     text: 'Why do you keep touching the cactus? It very obviously causes you pain.',
     id: '9a1a0176-2fd8-4842-9a59-6a1139d773f3',
   },
   {
-    clicks: 50,
+    clicks: 100,
     text: 'The sharp pain in your finger does not stop you from touching the cactus over and over again.',
     id: 'bc477d8e-1cb6-4fa3-a05a-1a652d1af511',
   },
   {
-    clicks: 75,
+    clicks: 150,
     text: 'In fact, you touch it faster. And faster. And faster.',
     id: 'c4b46984-c79d-4e53-a49a-30c42db53ef8',
   },
   {
-    clicks: 100,
-    text: '...and here we are, one hundred puncture wounds on your hand later.',
+    clicks: 200,
+    text: '...and here we are. Two hundred puncture wounds later.',
     id: '96555234-1a8d-4de2-889a-58a272889c6b',
   },
   {
-    clicks: 130,
+    clicks: 250,
     text: 'You are aware that touching it over and over makes you look like a fool, right?',
     id: '9a688ea6-6812-41f4-9886-d99e922c8b95',
   },
   {
-    clicks: 160,
+    clicks: 300,
     text: 'No glory awaits you. Only pain and suffering.',
     id: 'ed58ad3a-07db-4ffa-9b1e-fec012e31848',
   },
   {
-    clicks: 180,
+    clicks: 350,
     // eslint-disable-next-line prettier/prettier
     text: 'No, you won\'t get a medal for this.',
     id: '8e8a9a4b-3827-4217-b09a-4795216a0da1',
   },
   {
-    clicks: 200,
+    clicks: 400,
     text: 'There is no trophy for puncturing your hand two hundred times, either.',
     id: 'ca874b12-45d7-415e-b68a-ebf772b7052a',
   },
   {
-    clicks: 230,
+    clicks: 450,
     text: 'Look at you, sitting here, expressionless, disturbing the continuity of your skin, for absolutely no reward whatsoever.',
     id: '2b45c5db-229b-405c-be70-09c834d123db',
   },
   {
-    clicks: 260,
+    clicks: 500,
     text: 'In fact, doing it for any kind of reward would be pretty dumb as well.',
     id: '2a3ad955-185b-4f04-82f4-e03ea2edc401',
   },
   {
-    clicks: 290,
+    clicks: 550,
     text: 'There is this neat little concept called honor and dignity, you know.',
     id: 'b3d37614-c0c2-48b4-857a-b6e5aa4bc5c3',
   },
   {
-    clicks: 320,
+    clicks: 600,
     text: 'It kiiiind of revolves around not humiliating yourself with some dumb activity, like stabbing yourself repeatedly with a god-forsaken cactus.',
     id: '012086cf-dfad-4155-89b4-75269ea9877d',
   },
   {
-    clicks: 350,
+    clicks: 650,
     text: 'Oh, did that example sound... oddly specific? Good. It was supposed to.',
     id: '5f4a1b0e-6cb5-42a1-844c-6b9ec430fa1b',
   },
   {
-    clicks: 380,
+    clicks: 700,
     text: 'But I suppose there is no point explaining that to a person willing to put themselves through this kind of activity, you know?',
     id: '205e3b45-f84f-42c9-b7c9-c6fc0fb65234',
   },
   {
-    clicks: 410,
+    clicks: 750,
     // eslint-disable-next-line prettier/prettier
     text: 'It\'s almost as if I was talking to a wall.',
     id: '40b62594-a1fb-4431-acaa-8178594080ff',
   },
   {
-    clicks: 440,
+    clicks: 800,
     text: 'Wall covered with an extraordinary amount of holes, mind you.',
     id: '19b30374-9126-471f-a4e4-8debbe6defc4',
   },
   {
-    clicks: 470,
+    clicks: 850,
     // eslint-disable-next-line prettier/prettier
     text: 'I hope you don\'t have any people around who could suffer from tryphophobia.',
     id: 'ad986ad6-8a7c-4570-ba96-d814c0c51994',
   },
   {
-    clicks: 500,
-    text: 'Who am I kidding, who in their right mind would like to spend their time around a cactus toucher. You did that over five hundred times already, by the way.',
+    clicks: 900,
+    text: 'Who am I kidding, who in their right mind would like to spend their time around a cactus toucher. You did that over nine hundred times already, by the way.',
     id: 'f15e0068-26a2-43c1-95c3-5613678a1124',
   },
   {
-    clicks: 530,
+    clicks: 950,
     text: 'By the way, it seems that your masochistic tendencies cause the cactus to grow. It pretty much covers your entire screen already.',
     id: '915e800b-e010-4b6d-b3d9-7530fe88225b',
   },
   {
-    clicks: 560,
+    clicks: 1000,
     // eslint-disable-next-line prettier/prettier
     text: 'Soon you won\'t be able to see the number of clicks anymore. It will ultimately remove any remaining reason to click.',
     id: 'a09baeb1-624f-4d19-862c-a50d12a6712c',
   },
   {
-    clicks: 590,
+    clicks: 1050,
     text: 'What a cruel irony; a pointless activity recursively causing itself to be even more pointless. That would be sad if it was not so pointless.',
     id: '6f3922da-7961-41e1-bc61-3d0b90ee8d7a',
   },
   {
-    clicks: 620,
+    clicks: 1100,
     text: 'I pity you. I truly, truly, truly pity you.',
     id: '8ed387a5-db4f-4ee1-bb21-938432b5fd49',
   },
   {
-    clicks: 650,
+    clicks: 1150,
     // eslint-disable-next-line prettier/prettier
     text: 'In fact, I pity you so much that I think it\'s time to spice up your little activity a little bit.',
     id: '3aaa17db-981a-4b98-94ce-c56492019fdd',
   },
   {
-    clicks: 680,
+    clicks: 1200,
     text: 'Here you go! A brand new thing to hurt yourself with.',
     id: '76313b9f-d4c0-445c-b2ba-90fa789f0306',
   },
   {
-    clicks: 710,
+    clicks: 1250,
     text: 'And it has much higher density of spikes. Your masochistic heart must now be crying of joy.',
     id: 'faec5489-7165-460b-9540-72ae14e21ea5',
   },
   {
-    clicks: 740,
+    clicks: 1300,
     // eslint-disable-next-line prettier/prettier
-    text: 'Or crying of pain. I don\'t judge.',
+    text: 'Or... crying of pain. I don\'t judge.',
     id: '452c10f1-b221-46b0-9c4e-6df2bbc0938d',
   },
   {
-    clicks: 770,
+    clicks: 1350,
     // eslint-disable-next-line prettier/prettier
     text: 'Actually I DO judge, but that\'s besides the point.',
     id: '6dcd755a-1173-4041-bdcc-8fcaa36a16eb',
   },
   {
-    clicks: 800,
+    clicks: 1400,
     text: 'Our little spikey friend does not really look amused, though. Perhaps it does not fancy being touched.',
     id: 'f9cfa4cb-9d2c-4d8b-9967-388b2d41cd21',
   },
   {
-    clicks: 830,
+    clicks: 1450,
     text: 'Yes, it certainly does not like to be touched. Otherwise it would not cover itself with such a quantity of little needles.',
     id: 'b08e26bd-4da0-4909-b377-08bbc6d23718',
   },
   {
-    clicks: 860,
+    clicks: 1500,
     text: 'Turns out spikes are nature\'s subtle way of communicating "No! Do not touch me! I hate being touched! Go away!"',
     id: '9eafc53b-7f3e-4981-9558-2eaf3e8d7e84',
   },
   {
-    clicks: 890,
+    clicks: 1550,
     text: '...',
     id: '7841f4e3-026b-47f1-8f74-758632673968',
   },
   {
-    clicks: 920,
+    clicks: 1600,
     // eslint-disable-next-line prettier/prettier
     text: '...and yet, knowing that, you\'re still touching that poor hedgehog. You\'re the most thickskulled individual I\'ve ever encountered.',
     id: 'c739487a-caa9-4dcd-9e2e-1778c2f3cda9',
   },
   {
-    clicks: 950,
+    clicks: 1650,
     text: 'Look at it. Look in its eyes. Do you see confusion? Do you see fear?',
     id: '040fdd5b-fd44-441e-aca2-2a1228193829',
   },
   {
-    clicks: 980,
+    clicks: 1700,
     // eslint-disable-next-line prettier/prettier
     text: 'You\'re purposedly causing it discomfort despite there being no discernible reward for it. I\'m afraid you might be a psychopath.',
     id: '43a0efc0-08a1-416c-b3f5-8b1c28ddc758',
   },
   {
-    clicks: 1000,
+    clicks: 1750,
     // eslint-disable-next-line prettier/prettier
     text: 'This is your 1000th time touching a spiked thing, and we aren\'t even a bit closer to an end.',
     id: 'c8716bd5-e1f3-4aa3-8935-cc4124480e8a',
   },
   {
-    clicks: 1030,
+    clicks: 1800,
     text: 'It looks like the leaking from your countless holes is nourishing any living being you touch, because our little friend stopped being "little" quite a bit of time ago.',
     id: '8b5a6012-1dad-4441-8844-072f7e318f02',
   },
   {
-    clicks: 1060,
+    clicks: 1850,
     // eslint-disable-next-line prettier/prettier
     text: 'It\'s a plausible theory, isn\'t it? What could be another reason for this glorious phenomena?',
     id: 'd0be5878-b1db-489e-ae8c-7a09c50f17b1',
   },
   {
-    clicks: 1090,
+    clicks: 1900,
     text: 'Maybe if the thing you touch is not an organic entity, it will stay with its original size and you can touch it till the end of the world.',
     id: 'f6d0c5d2-7d49-471b-987f-8d9d647f2950',
   },
   {
-    clicks: 1120,
+    clicks: 1950,
     text: '...or until you perish from blood loss, what is likely to happen quite soon, judging by the state of your hands.',
     id: '124a92ff-3a95-4a09-9e97-cd7a82a75d12',
   },
   {
-    clicks: 1150,
+    clicks: 2000,
     text: 'Ta-daa! BEHOLD MY NEWEST CREATION!',
     id: '0d41c302-6187-4bd1-8956-f9aa30d67da0',
   },
   {
-    clicks: 1180,
+    clicks: 2050,
     text: 'Look at this beautiful roll of barbed wire. So pure, so beautiful... So spikey.',
     id: '76fe0cda-d22d-462c-9a6e-1db220d3942d',
   },
   {
-    clicks: 1210,
+    clicks: 2100,
     // eslint-disable-next-line prettier/prettier
     text: 'Given how clueless you\'ve proven to be so far, I think I owe you a little bit of context with this one.',
     id: 'f219d427-fb6b-4597-a660-6303da388fa2',
   },
   {
-    clicks: 1240,
+    clicks: 2150,
     text: 'Barbed wire, also known as barb wire,[1] is a type of steel fencing wire constructed with sharp edges or points arranged at intervals along the strands. It is used to construct inexpensive fences and is used atop walls surrounding secured property. It is also a major feature of the fortifications in trench warfare (as a wire obstacle).',
     id: 'b7e490c3-bf27-4b01-afbe-7d2640bd363f',
   },
   {
-    clicks: 1280,
+    clicks: 2250,
     text: 'This is Wikipedian definition of a "barbed wire". You can derive from this definition that "barbed wire" is used as a FENCE, OBSTACLE or a part of FORTIFICATION around a SECURED property.',
     id: 'dc96c642-6179-447c-8d1c-5e02c731f5d1',
   },
   {
-    clicks: 1310,
+    clicks: 2300,
     // eslint-disable-next-line prettier/prettier
     text: 'Now, I don\'t blame you if you still don\'t get it, but barbed wire is literally created to make people abstain from touching whatever is behind it, and by extension, also the barbed wire itself.',
     id: '9a36ad56-e193-4b09-b982-2629e5b61859',
   },
   {
-    clicks: 1340,
+    clicks: 2350,
     text: 'So what you are doing right now is legally prohibited by the VERY definition of the entity you are touching. And you show no intention to stop.',
     id: '9c1d73d0-e536-4eb0-a4bf-64da38907f28',
   },
   {
-    clicks: 1370,
+    clicks: 2400,
     text: 'Okay, you might be a bit tired from the blood loss, so I will repeat once again.',
     id: '8563287c-1787-4171-aa8f-f94d4b60953f',
   },
   {
-    clicks: 1400,
+    clicks: 2450,
     text: 'BARBED WIRE, ALSO KNOWN AS BARB WIRE,[1] IS A TYPE OF STEEL FENCING WIRE CONSTRUCTED WITH SHARP EDGES OR POINTS ARRANGED AT INTERVALS ALONG THE STRANDS. IT IS USED TO CONSTRUCT INEXPENSIVE FENCES AND IS USED ATOP WALLS SURROUNDING SECURED PROPERTY. IT IS ALSO A MAJOR FEATURE OF THE FORTIFICATIONS IN TRENCH WARFARE (AS A WIRE OBSTACLE).',
     id: '101bdca6-7f05-41cd-a905-b37b312ee452',
   },
   {
-    clicks: 1430,
+    clicks: 2500,
     text: '...nope. Still touching.',
     id: 'cda34cb6-06e5-42d0-9e4b-71ab9f2a4365',
   },
   {
-    clicks: 1470,
+    clicks: 2550,
     text: 'On the other hand, it seems like the barbed wire is also growing with your every touch.',
     id: '9863d096-271f-4949-8ddb-35563f13807c',
   },
   {
-    clicks: 1500,
+    clicks: 2600,
     text: 'And it seems that it grows a bit faster than the organic matter. How curious.',
     id: '1a0266f8-5da0-4522-b24d-92854a0d1d4c',
   },
   {
-    clicks: 1530,
+    clicks: 2650,
     // eslint-disable-next-line prettier/prettier
     text: 'Okay, let\'s try once again with a smaller amount.',
     id: '296b4553-b29f-49b2-a157-9a73d7792131',
   },
   {
-    clicks: 1560,
+    clicks: 2700,
     // eslint-disable-next-line prettier/prettier
     text: 'Now don\'t let it fool you: it\'s not as dense as the roll of barbed wire, but still as deadly.',
     id: 'f54a51e2-c4b6-44dd-a9e1-bca7c6fc6281',
   },
   {
-    clicks: 1590,
+    clicks: 2750,
     // eslint-disable-next-line prettier/prettier
     text: 'It\'s a razor wire instead of a barbed one, so it\'s more like slicey-slicey than spikey-spikey, but it doesn\'t make any difference, as you are still going to touch it.',
     id: '5d602b7e-3adf-40a4-be52-eb95f4e73e15',
   },
   {
-    clicks: 1620,
+    clicks: 2800,
     text: 'It grows even quicker than the previous one, though. This is getting kinda scary.',
     id: 'b3c58fbe-1a38-46d4-8384-aa48ccddd86c',
   },
   {
-    clicks: 1650,
+    clicks: 2850,
     // eslint-disable-next-line prettier/prettier
     text: 'Dunno about you, but I don\'t fancy happily spawning gigantic pain devices. It does not seem very responsible.',
     id: '48de826d-f9a9-4289-9a88-c8668aa75e0c',
   },
   {
-    clicks: 1680,
+    clicks: 2900,
     // eslint-disable-next-line prettier/prettier
     text: '...not that it\'s going to stop you.',
     id: '898dd9de-ee42-47d9-8a51-bc1e26521c47',
   },
   {
-    clicks: 1710,
+    clicks: 2950,
     text: 'Since you are obviously incapable of taking responsible adult decisions, it seems like I have to do it myself.',
     id: '0419d777-a55b-4beb-bb7f-331c8988df19',
   },
   {
-    clicks: 1740,
+    clicks: 3000,
     text: 'Here, have this sea urchin. Touch it as much as you want, while I go look for something less... hurt-y.',
     id: '0463e39e-f663-4407-8e14-dcb6973d0efd',
   },
   {
-    clicks: 1770,
+    clicks: 3050,
     // eslint-disable-next-line prettier/prettier
     text: 'Hmm... Nope, this won\'t do...',
     id: '7c23295d-e932-485d-aecf-1830767a36a7',
   },
   {
-    clicks: 1800,
+    clicks: 3100,
     text: '...this porcupine does not look like it wants to have anything to do with you...',
     id: 'bef0cc6a-2e46-4968-b779-b8c3757c2b93',
   },
   {
-    clicks: 1830,
+    clicks: 3150,
     // eslint-disable-next-line prettier/prettier
     text: 'Frankly, I kinda feel him. You\'re like cursed kind Midas, but instead of turning things to gold, you turn them huge as long as they hurt you.',
     id: '5c1cfa5f-7a87-424c-94c5-cca8deb23286',
   },
   {
-    clicks: 1860,
+    clicks: 3200,
     // eslint-disable-next-line prettier/prettier
     text: 'Why can\'t you just turn all of those spikey things into gold? That would be a great business plan.',
     id: 'ba568fdd-2938-4dce-a204-3c92e73d38d9',
   },
   {
-    clicks: 1890,
+    clicks: 3250,
     text: 'We would be absolutely filthy rich, given how much you enjoy poking stuff with your fingers. We would become decillionaries.',
     id: 'f561025c-da60-454b-b9ef-278add3f39e5',
   },
   {
-    clicks: 1910,
+    clicks: 3300,
     // eslint-disable-next-line prettier/prettier
     text: 'I\'m not even sure if "decillionare" is an actual word. I don\'t think anyone has amassed this amount of wealth, ever, so there isn\'t even a legit word for that.',
     id: 'b5ee675c-646a-4e78-a0d7-d49632852433',
   },
   {
-    clicks: 1940,
+    clicks: 3350,
     // eslint-disable-next-line prettier/prettier
     text: 'But nooooo, your superpower is to make spikey things bigger and bigger, hurting yourself in the process. That\'s most likely the shittiest superpower I\'ve ever heard of.',
     id: '75e45db6-2575-40e6-bccd-374db589f8f8',
   },
   {
-    clicks: 1970,
+    clicks: 3400,
     // eslint-disable-next-line prettier/prettier
     text: '...unless... The thing you touch does not need to be spikey. We did\'t actually test anything with no spikes at all.',
     id: 'e7d3a59a-9ced-4314-820a-62efb0960e67',
   },
   {
-    clicks: 2000,
+    clicks: 3450,
     // eslint-disable-next-line prettier/prettier
-    text: 'A small digression: you already touched items I gave you two thousand times. Now imagine you were turning things to gold instead, for goddamn\'s sake.',
+    text: 'A small digression: you already touched items I gave you almost three and a half thousand times. Now imagine you were turning things to gold instead, for goddamn\'s sake.',
     id: '04673fa2-200c-4ccf-a7d2-0421ae962cc7',
   },
   {
-    clicks: 2030,
+    clicks: 3500,
     // eslint-disable-next-line prettier/prettier
     text: 'Okay, so let\'s find you a harmless thing to touch. Not spikey, not dangerous when grown to a huge size.',
     id: 'd3fdfd8a-0cea-4426-b303-5c3c7eeeab23',
   },
   {
-    clicks: 2060,
+    clicks: 3550,
     text: 'Hmm...',
     id: '170ffebd-bc40-4bc6-96f6-840f03d6cf81',
   },
   {
-    clicks: 2090,
+    clicks: 3600,
     text: '...if you expected a dirty joke here, I have to disappoint you. Just like you are a disappointment to your parents.',
     id: '1923a343-ca6b-4428-a180-ea50b6ca1736',
   },
   {
-    clicks: 2120,
+    clicks: 3650,
     // eslint-disable-next-line prettier/prettier
     text: 'This seems harmless enough. Let\'s give it a try.',
     id: 'b0f2cbb3-cf03-4bdf-b94b-43076f0bbd92',
   },
   {
-    clicks: 2150,
+    clicks: 3700,
     text: 'Hah! AND you finally shut up. The sound of silence is music to my ears. No more pointless grunts of pain.',
     id: '33baaf59-546d-4e78-8a9f-c4d110b9cc6c',
   },
   {
-    clicks: 2180,
+    clicks: 3750,
     text: 'It does not seem like the teddy is growing, like at all.',
     id: '6ee819b9-ad43-42e6-b27e-b269107a870b',
   },
   {
-    clicks: 2210,
+    clicks: 3800,
     // eslint-disable-next-line prettier/prettier
     text: 'Let\'s give it a few more touches. For science.',
     id: '9e3e0658-c47c-4c2a-9ad7-8efb0a680dac',
   },
   {
-    clicks: 2240,
+    clicks: 3850,
     text: 'Nope, it still stays the same size. Maybe it needs to be a biological matter after all.',
     id: '52e7d37e-0490-4a67-a3bf-3aaef9c0e9a6',
   },
   {
-    clicks: 2270,
+    clicks: 3900,
     text: 'Another test subject!',
     id: 'a9e59104-27c8-45e7-9705-90e06fc69b11',
   },
   {
-    clicks: 2300,
+    clicks: 3950,
     // eslint-disable-next-line prettier/prettier
     text: 'This is Harry. He\'s a very nice kitty. You are allowed to pet him. For science.',
     id: '7c44ab3b-b341-4b48-9e8b-946db36e9712',
   },
   {
-    clicks: 2330,
+    clicks: 4000,
     text: '...this is... not how you pet a cat. Petting a cat is done via gentle strokes in direction from head towards tail, alongside spine. What you\'re doing is "poking a cat".',
     id: '9d3cc9cf-2f4e-4814-bf6c-05f0efeb61ff',
   },
   {
-    clicks: 2360,
+    clicks: 4050,
     text: 'Poking a cat does not seem to wield any discernable results towards its unusual growth. Interesting, interesting.',
     id: 'e60248bf-b4c7-475a-b850-da8311176a72',
   },
   {
-    clicks: 2390,
+    clicks: 4100,
     text: 'Really, can you pet the cat instead? I think he is getting a bit angry at you poking him.',
     id: '7035707b-5515-4bbf-8448-55e8aabfb5f6',
   },
   {
-    clicks: 2420,
-    text: '...aaaand you made Harry angry.',
+    clicks: 4150,
+    text: '...aaaand you made him angry.',
     id: 'd24407b3-9cc4-421e-8de4-1d9a8799a289',
   },
   {
-    clicks: 2450,
+    clicks: 4200,
     text: 'I am not the type of person to tell you "I told you so", but I told you so. Cats are meant to be pet, not poked.',
     id: '825bbc30-7918-436d-8efc-d31df5a91957',
   },
   {
-    clicks: 2480,
+    clicks: 4250,
     // eslint-disable-next-line prettier/prettier
     text: 'Holy crap, he\'s growing so fast! Abort! ABORT!',
     id: '7cfc9e30-b586-4ed4-965a-14c7b5357ced',
   },
   {
-    clicks: 2500,
+    clicks: 4300,
     text: 'MEEEEEEEEEEEEEOOOOOOOOOOOW',
     id: '78ac028e-83c1-412b-93ac-534034d2f46f',
   },
